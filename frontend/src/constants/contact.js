4970b418dd74492e15120d93caf5209da61d65ca
Ajay <gangwar030@gmail.com>
2026-07-30 18:57:47 +0530

    Contact Section

---
+++ frontend/src/constants/contact.js
@@ -0,0 +1,6 @@
+// Single place to change StubLine's public-facing contact addresses.
+// Currently pointed at the same inbox the backend sends OTP/booking emails
+// from (spring.mail.username) - swap these if you want feedback/support to
+// go to a different address than the transactional sender.
+export const SUPPORT_EMAIL = 'gangwar030@gmail.com'
+export const FEEDBACK_EMAIL = 'gangwar030@gmail.com'
