264f50d5488ce2d9ab5e06d5347ff9a8984d24c9
Ajay <gangwar030@gmail.com>
2026-08-14 00:12:18 +0530

    Announcement Repository

---
+++ backend/src/main/java/com/eventbooking/repository/AnnouncementRepository.java
@@ -0,0 +1,11 @@
+package com.eventbooking.repository;
+
+import com.eventbooking.entity.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+    List<Announcement> findByActiveTrueOrderByCreatedAtDesc();
+    List<Announcement> findAllByOrderByCreatedAtDesc();
+}
