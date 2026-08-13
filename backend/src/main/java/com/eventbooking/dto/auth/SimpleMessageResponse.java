3f8f8362d91a6ed10f55c86e9c81a6f850b15108
Ajay <gangwar030@gmail.com>
2026-08-14 00:18:48 +0530

    Simple Message Response

---
+++ backend/src/main/java/com/eventbooking/dto/auth/SimpleMessageResponse.java
@@ -0,0 +1,14 @@
+package com.eventbooking.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleMessageResponse {
+    private String message;
+}
